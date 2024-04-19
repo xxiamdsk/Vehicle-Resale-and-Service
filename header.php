fa5aeed0227fd7233003510f87f222badbf95d59
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2024-04-19 22:14:23 +0530

    new update

---
+++ header.php
@@ -0,0 +1,73 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-light navigation">
+                    <a class="navbar-brand" href="index.php">
+                        <img src="images/wheelXchange(1).png" alt="logo" style="height: 40px ; width: 150px;">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto main-nav ">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link"
+                                    href="cars.php">Buy
+                                </a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link"
+                                    href="#!">Sell
+                                </a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link"
+                                    href="Service.php">Service
+                                </a>
+                            </li>
+                            <li class="nav-item dropdown dropdown-slide @@dashboard">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown"
+                                    href="#!">Dashboard<span><i class="fa fa-angle-down"></i></span>
+                                </a>
+
+                                <!-- Dropdown list -->
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item " href="dashboard.html">Dashboard</a>
+                                    </li>
+                                    <li><a class="dropdown-item @@dashboardMessages"
+                                            href="dashboard-my-ads.html">My Cars</a></li>
+                                    <li><a class="dropdown-item @@dashboardFavouriteAds"
+                                            href="dashboard-favourite-ads.html"> Favourite Car</a></li>
+                                    <li class="dropdown dropdown-submenu dropright">
+                                        <a class="dropdown-item dropdown-toggle" href="#!" id="dropdown0501"
+                                            role="button" data-toggle="dropdown" aria-haspopup="true"
+                                            aria-expanded="false">Sub Menu</a>
+
+                                        <ul class="dropdown-menu" aria-labelledby="dropdown0501">
+                                            <li><a class="dropdown-item" href="index.html">Submenu 01</a></li>
+                                            <li><a class="dropdown-item" href="index.html">Submenu 02</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ml-auto mt-10">
+                            <li class="nav-item">
+                                <a class="nav-link login-button" href="login.php">Login</a>
+                            </li>
+                            <!-- <li class="nav-item">
+                            <a class="nav-link text-white add-button" href="ad-listing.html"><i class="fa fa-plus-circle"></i> Add Listing</a>
+                        </li> -->
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
